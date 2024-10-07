d7c030c00da0e0c6c6bc4a043b454995118f3389
Wahyu Agung Utomo <wagung928@gmail.com>
2024-10-07 12:02:18 +0700

    Add AC-XXXX

---
+++ Downloads/blazor-kanban-board-features-master v1.1/Test Sigmatech 2024-10-02 v.1.0/WEB/Data/KanbanData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CreatingKanbanSample.Data
+{
+    public class KanbanModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Status { get; set; }
+        public string Summary { get; set; }
+    }
+}
